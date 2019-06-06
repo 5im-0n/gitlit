1718c7652e4218be0403e9d576c578dd341b6426
s2 <s2@31337.it>
2019-06-06 17:06:25 +0200

    fix args

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ const args = require('minimist')(process.defaultApp ? process.argv.slice(3) : pr
 	}
 });
 
-const repoDir = path.resolve(path.normalize(args._[0]));
+const repoDir = path.resolve(path.normalize(args._.join(' ')));
 
 function getLfsFileList(dir, cb) {
 	exec('git lfs ls-files', {
